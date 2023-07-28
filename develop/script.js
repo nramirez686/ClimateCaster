3b0dec3d2c41df9ce6497a0f82a9291310d869cf
Fancy Nancy <ramireznancy.686@gmail.com>
2023-07-27 21:24:49 -0600

    changed html

--- develop/script.js
+++ develop/script.js
@@ -1,26 +1,14 @@
-// Get references to HTML elements
-const form = document.getElementById("search-form");
-const cityInput = document.getElementById("city-input");
-const weatherInfo = document.getElementById("weather-info");
-const forecastInfo = document.getElementById("forecast-info");
-const searchHistory = document.getElementById("search-history");
-
-const apiKey = "97cfd0909846f759120be372471c2a97";
-
-let searchHistoryData = [];
-
-function fetchWeatherData(city) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
-
-  return fetch(apiUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      console.error("Error fetching weather data:", error);
-      return null;
+function getWeatherData(location) {
+  const apiKey = "97cfd0909846f759120be372471c2a97";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weatherData = {
+        temperature: data.main.temp,
+        condition: data.weather[0].main,
+        location: data.name,
+      };
+      return weatherData;
     });
 }
